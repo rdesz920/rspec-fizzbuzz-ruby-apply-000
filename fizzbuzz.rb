70f19e5fea104f4e8296062d8b3a8a89a01840f4
Rob Desz <rob.desz@gmail.com>
2020-10-10 19:16:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz (int)
-  if int % 3 == 3 && int % 5 == 5
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  elsif int % 3 == 3
+  elsif int % 3 == 0
     "Fizz"
-  elsif int % 5 == 5
+  elsif int % 5 == 0
     "Buzz"
   end
 end
+
